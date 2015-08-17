4a95509c5d1f797231f2c9e5208afe6e28b9819d
Clemens Fehr <clemens.fehr@sap.com>
2015-08-17 16:00:33 +0200

    Generated Win32 App plus Debug Heap Test

---
+++ ShadedPath12/ShadedPath12/stdafx.h
@@ -0,0 +1,30 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#define _CRTDBG_MAP_ALLOC
+#ifdef _DEBUG
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
+#endif
+#endif  // _DEBUG
+#include <crtdbg.h>
+
+
+// TODO: reference additional headers your program requires here
