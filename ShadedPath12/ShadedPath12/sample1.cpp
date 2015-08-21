e963a255ae66e8c5b9e74222320ac193dd6fdd89
Clemens Fehr <clemens.fehr@sap.com>
2015-08-21 07:32:51 +0200

    forgotten file

---
+++ ShadedPath12/ShadedPath12/sample1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "sample1.h"
+
+
+Sample1::Sample1() : XAppBase()
+{
+	myClass = string(typeid(*this).name());
+	xapp().registerApp(myClass, this);
+}
+
+
+Sample1::~Sample1()
+{
+}
+
+string Sample1::getWindowTitle() {
+	return "Text Sample";
+}
+
+static Sample1 sample1;
