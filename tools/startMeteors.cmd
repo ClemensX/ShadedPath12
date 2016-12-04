0f9caac98a70e46a4fccef29c9949e597c062da8
Clemens Fehr <clemensfehr@gmx.de>
2016-12-04 20:16:08 +0100

    add cmd files to start demos

---
+++ tools/startMeteors.cmd
@@ -0,0 +1,2 @@
+cd ..\ShadedPath12\x64
+.\Release\ShadedPath12.exe -vrX -app=HangOn -w=1200 -h=676 -disableLineShadersX
---
+++ tools/startMeteorsVR.cmd
@@ -0,0 +1,2 @@
+cd ..\ShadedPath12\x64
+.\Release\ShadedPath12.exe -vr -app=HangOn -w=1200 -h=676 -disableLineShadersX
