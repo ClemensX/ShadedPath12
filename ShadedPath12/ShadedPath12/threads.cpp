a7b9900f8f89199394f38ccbcff4bf419199e965
Clemens Fehr <clemensfehr@gmx.de>
2018-01-25 06:41:08 +0100

    more logs

--- ShadedPath12/ShadedPath12/threads.cpp
+++ ShadedPath12/ShadedPath12/threads.cpp
@@ -40,6 +40,7 @@ void Command::renderQueueTask(XApp * xapp)
 			if (true) {
 				//xapp->dxmanager.waitGPU(*command.frameResource, xapp->appWindow.commandQueue);
 				xapp->appWindow.commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+				Log("present swap chain " << xapp->appWindow.swapChain->GetCurrentBackBufferIndex() << endl);
 				ThrowIfFailedWithDevice(xapp->appWindow.swapChain->Present(0, DXGI_PRESENT_ALLOW_TEARING), xapp->device.Get());
 				Log("render queue t = " << ThreadInfo::thread_osid() << " render command frame: " << render_command_frame << " presented " << endl);
 				// unlock this slot to enable next draw call
