e003c983c7c623865da42c774823b952b5351b8c
Clemens Fehr <clemens.fehr@sap.com>
2019-06-17 15:57:07 +0200

    temp

--- ShadedPath12/ShadedPath12Lib/effect.h
+++ ShadedPath12/ShadedPath12Lib/effect.h
@@ -1,4 +1,32 @@
+// base class for App Data
+class EffectAppData {
+	virtual ~EffectAppData() = 0;
+};
+
+// base class for Per Frame Data
+class EffectFrameData {
+	virtual ~EffectFrameData() = 0;
+};
+
 // base class for effects
+
 class Effect {
-	
+	// effects need 3 kinds of data:
+	// 1) Global data for the effect, only needed once. Like PipelineState and RootSignature
+	// 2) Application data thats resembles the application state with regards for an effect. Like position and normal for billboard elements
+	//    Needs to be there twice: there is alway an acive data set used for rendering, the inacive set can be manipulated by the application until it is uploaded to GPU and made active
+	// 3) Per Frame data. Usually 3 sets. Frame specific resources the effect needs to run in parallel, like CBVs with per frame WVP matrix
+
+public:
+	// get inactive data set. It can be changed by effect or app code
+	virtual EffectAppData* getInactiveAppDataSet() = 0;
+	// get data set that is currently used by render code, should never be changed
+	virtual EffectAppData* getActiveAppDataSet() = 0;
+	// activate the curerently inactive data set. Includes uploading to GPU
+	// rendering after this call returns will use the new data set. 
+	// returns nullptr if there is no active set yet
+	virtual void activateAppDataSet() = 0;
+
+	virtual ~Effect() = 0;
+
 };
