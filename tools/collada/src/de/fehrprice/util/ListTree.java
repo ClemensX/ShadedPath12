f5d945c83ad05c9f759c87eebd6a229a14061206
Clemens Fehr <clemensfehr@gmx.de>
2020-03-23 07:41:24 +0100

    ListTree

--- tools/collada/src/de/fehrprice/util/ListTree.java
+++ tools/collada/src/de/fehrprice/util/ListTree.java
@@ -2,53 +2,95 @@ package de.fehrprice.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import de.fehrprice.util.TestTree.Anything;
 
 /**
- * ListTree - build tree structure from simple lists. Preserve order for added nodes on same level. No restructuring. No element deletion.
+ * ListTree - build tree structure from simple lists. Preserve order for added
+ * nodes on same level. No restructuring. No element deletion.
  *
  * @param <T>
  */
 public class ListTree<T> {
-	public class ListTreeNode<N> {
+	public static class ListTreeNode<N> {
 		N e = null;
+		int layer; // root is layer 0
 		ListTreeNode<N> parent = null;
 		List<ListTreeNode<N>> children = new ArrayList<>();
+		public Stream<ListTreeNode<N>> flattenedTopDown() {
+			return Stream.concat(Stream.of(this), children.stream().flatMap(ListTreeNode::flattenedTopDown));
+		}
+		public N get() {
+			return e;
+		}
 	}
-	
+
 	private ListTreeNode<T> root = null;
 
 	// create new tree with empty root element
 	public ListTree() {
 		root = new ListTreeNode<T>();
 		root.e = null;
+		root.layer = 0;
 	}
-	
+
 	// create new tree and set root element
 	public ListTree(T e) {
-		root = new ListTreeNode<T>();
+		super();
 		root.e = e;
 	}
-	
+
 	// check if tree is empty (containes no elemnts)
 	public boolean isEmpty() {
 		return root.e == null && root.children.size() == 0;
 	}
-	
+
 	// empty tree structure
 	public void empty() {
 		this.root = new ListTreeNode<T>();
 	}
+	
+	public ListTreeNode<T> getRoot() {
+		return root;
+	}
 
 	/**
 	 * Add element to parent node. Element will be appended to and of children list.
-	 * @param node if node is null element will be appended under root node
+	 * 
+	 * @param parent if node is null element will be appended under root node
 	 * @param a
+	 * @return tree node at insertion point
 	 */
-	public void add(ListTreeNode<T> node, T a) {
-		// TODO Auto-generated method stub
+	public ListTreeNode<T> add(ListTreeNode<T> parent, T a) {
+		if (parent == null) {
+			parent = getRoot();
+		}
+		ListTreeNode<T> n = new ListTreeNode<T>();
+		n.e = a;
+		n.parent = parent;
+		n.layer = parent.layer + 1;
+		parent.children.add(n);
+		return n;
+	}
+
+	public Stream<ListTreeNode<T>> flattenedTopDown() {
+		return root.flattenedTopDown();
+	}
+	
+	public List<T> getElementsTopDown() {
+		return flattenedTopDown().map(ListTree.ListTreeNode::get).filter(Objects::nonNull).collect(Collectors.toList());
+	}
+
+	public List<ListTreeNode<T>> getNodesTopDown() {
+		return flattenedTopDown().filter(Objects::nonNull).collect(Collectors.toList());
+	}
+
+	public void printNodesTopDown(Consumer<T> printMethod) {
+		//t -> { printMethod.accept(t); }
 		
 	}
 }
-
--- tools/collada/src/de/fehrprice/util/TestTree.java
+++ tools/collada/src/de/fehrprice/util/TestTree.java
@@ -1,8 +1,16 @@
 package de.fehrprice.util;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
+
+import de.fehrprice.util.ListTree.ListTreeNode;
 
 /**
  * 
@@ -13,6 +21,9 @@ class TestTree {
 	public class Anything {
 		public int num;
 		public String name; 
+		public String print() {
+			return "" + num;
+		}
 	}
 	
 	@Test
@@ -24,8 +35,37 @@ class TestTree {
 		
 		Anything a = new Anything();
 		a.num = num++;
+		ListTreeNode<Anything> e1 = tree.add(null, a);
+		a = new Anything();
+		a.num = num++;
+		tree.add(null, a); // 2nd child of root
 		
-		tree.add(null, a);
+		a = new Anything();
+		a.num = num++;
+		tree.add(e1, a); // one child at first element under root 
+		
+		//List<Anything> l = tree.flattenedTopDown().map(ListTree.ListTreeNode::get).filter(Objects::nonNull).collect(Collectors.toList());
+		List<Anything> l = tree.getElementsTopDown();
+		assertTrue(l.size() == 3);
+		
+		String s = "";
+		for (Anything any : l) {
+			s += any.num + " ";
+		}
+		System.out.println(s);
+		assertEquals("1 3 2 ", s);
+		
+		List<ListTreeNode<Anything>> ln = tree.getNodesTopDown();
+		for (ListTreeNode<Anything> any : ln) {
+			//String spaces = String.format("%1$"+(any.layer)+"s", "");
+			int count = any.layer+1;
+			String spaces = String.format("%"+ count +"s", " ");
+			//String spaces = String.format("%1$"+count+"s", "");
+			String v = any.e == null ? "root" : ""+any.e.num;
+			System.out.println(spaces + v);
+		}
+		
+		tree.printNodesTopDown(Anything::print);
 	}
 
 }
